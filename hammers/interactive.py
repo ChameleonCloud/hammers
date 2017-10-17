b833d8c2c8331e3c767811acae487fe31a7f56d6
Nick Timkovich <npt@uchicago.edu>
2017-10-17 18:00:46 -0500

    Interactive utilities for workbooks

---
+++ hammers/interactive.py
@@ -0,0 +1,33 @@
+import json
+import pathlib
+
+from . import osapi
+
+
+CC_LOCAL = pathlib.Path.home() / '.config' / 'chameleoncloud.json'
+
+
+def auth(rc_file, retries=3, password=None):
+    for i in range(retries):
+        try:
+            with CC_LOCAL.open() as f:
+                settings = json.load(f)
+            password = settings['password']
+        except (OSError, KeyError):
+            pass
+
+        rc = osapi.load_osrc(rc_file, get_pass=(not password))
+        if password:
+            rc['OS_PASSWORD'] = password
+
+        # abort if blank pass entered
+        if rc['OS_PASSWORD'] == '':
+            print('Aborting.', file=sys.stderr)
+            return
+
+        try:
+            auth = osapi.Auth(rc)
+        except RuntimeError:
+            print('Auth failed.', file=sys.stderr)
+        else:
+            return auth
