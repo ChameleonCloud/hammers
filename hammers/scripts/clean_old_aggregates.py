e346aea94098fe7cc358fbbd339aeeb3af58f680
Cody Hammock <hammock@tacc.utexas.edu>
2019-05-28 15:17:47 -0500

    add slack notification to script

--- hammers/scripts/clean_old_aggregates.py
+++ hammers/scripts/clean_old_aggregates.py
@@ -5,6 +5,7 @@ import argparse
 import os
 import itertools
 import re
+from hammers.slack import Slackbot
 from hammers import osapi, osrest
 from hammers.osrest.nova import aggregate_move_host
 from hammers.osrest.nova import aggregate_delete
@@ -35,6 +36,23 @@ def aggregates_for_lease(lease):
     physical_reservation_ids = [r['id'] for r in lease['reservations'] if r['resource_type'] == 'physical:host']
     return [x for x in aggregates.values() if x['name'] in physical_reservation_ids]
 
+def clear_aggregates(agg_list):
+
+    report = []
+    report.append("{:<45} {:<50}".format('ID','Message'))
+
+    for x in agg_list:   
+        if x['hosts']:   
+            for host in x['hosts']:                 
+                report.append("Deleting host {} from aggregate {} and returning to freepool".format(host, x['id']))
+                aggregate_move_host(auth, host, x['id'], 1) 
+            report.append("Deleting aggregate {}".format(host, x['id']))
+            aggregate_delete(auth, x['id'])
+
+    print report
+    return report
+
+
 def main(argv=None):
 
     term_leases = [lease for lease in leases.values() if is_terminated(lease)]
@@ -43,13 +61,16 @@ def main(argv=None):
 
     aggregate_list = list(itertools.chain(*old_aggregates))
 
-    for x in aggregate_list:
-        if x['hosts']:
-            for host in x['hosts']:
-                print("Deleting host {} from aggregate {} and returning to freepool".format(host, x['id']))
-                aggregate_move_host(auth, host, x['id'], 1)
-            print("Deleting aggregate {}".format(host, x['id']))
-            aggregate_delete(auth, x['id'])
+    agg_report = clear_aggregates(aggregate_list)
+
+    if args.slack:
+        slack = Slackbot(args.slack, script_name='clean-old-aggregates')
+
+    if slack:
+        if agg_report:
+            slack.post('clean-old-aggregates', agg_report, color='#FF0000')
+        else:
+            slack.post('clean-old-aggregates', 'Aggregates are clean', color='#000000')
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
