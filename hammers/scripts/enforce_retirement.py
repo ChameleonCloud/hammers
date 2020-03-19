4699681571771bd6d4f6fbc9463249086fb41f7a
Cody Hammock <hammock@tacc.utexas.edu>
2020-03-19 16:19:49 -0500

    Simplified mysql query

--- hammers/scripts/enforce_retirement.py
+++ hammers/scripts/enforce_retirement.py
@@ -3,39 +3,23 @@
 import mysql.connector
 import os
 from hammers.slack import Slackbot
-#from hammers.util import base_parser
-import sys
-sys.path.append('/root/fhalbach/hammers/hammers')
 from hammers.util import base_parser
 
 def correct_state(cursor,slk,dryrun=False):
-    '''
-    # Find retired nodes
-    cursor.execute("SELECT uuid FROM ironic.nodes WHERE name LIKE '%retired';")
-    retired_nodes = cursor.fetchall()
-
-    # Find blazar hosts with incorrect state
-    for node in retired_nodes:
-        blazar_chk = "SELECT reservable FROM blazar.computehosts WHERE hypervisor_hostname = %s"
-        cursor.execute(blazar_chk, [node[0]])
-        if cursor.fetchall()[0][0] != 0:
-    '''
     # Find retired nodes
     cursor.execute("SELECT n.uuid from ironic.nodes n join blazar.computehosts ch on n.uuid = ch.hypervisor_hostname WHERE n.name LIKE '%retired' AND ch.reservable != 0;")
     retired_nodes = cursor.fetchall()
-    print(retired_nodes)
-'''
-            if not dryrun:
-                blazar_fix = "UPDATE blazar.computehosts SET reservable = '0' WHERE hypervisor_hostname = %s"
-                cursor.execute(blazar_fix, [node[0]])
-                mess = ("Reverted state of node " + node[0] + " to non-reservable.")
-            else:
-                mess = ("State of retired node " + node[0] + " is reservable, run without '--dryrun' to retire.")
+    for node in retired_nodes:
+        if not dryrun:
+            blazar_fix = "UPDATE blazar.computehosts SET reservable = '0' WHERE hypervisor_hostname = %s"
+            cursor.execute(blazar_fix, [node[0]])
+            mess = ("Reverted state of node " + node[0] + " to non-reservable.")
+        else:
+            mess = ("State of retired node " + node[0] + " is reservable, run without '--dryrun' to retire.")
+        print(mess)
+        if slk:
+            slk.message(mess)
 
-            print(mess)
-            if slk:
-                slk.message(mess)
-'''
 def main(argv=None):
     if argv is None:
         argv = sys.argv
@@ -48,9 +32,9 @@ def main(argv=None):
     # Open MYSQL connection
     # Set MYSQL Login and host, create connection
     conn = mysql.connector.connect(
-      host = os.environ.get(MYSQL_HOST),
-      user = os.environ.get(MYSQL_USER),
-      passwd = os.environ.get(MYSQL_PASSWORD),
+      host = os.environ.get('MYSQL_HOST'),
+      user = os.environ.get('MYSQL_USER'),
+      passwd = os.environ.get('MYSQL_PASSWORD'),
     )
     
     mycursor = conn.cursor()
