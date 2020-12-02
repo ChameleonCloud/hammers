381af5c26a869c04c35f5348600e59a2c0faff60
Jason Anderson <jasonanderson@uchicago.edu>
2020-12-02 12:40:28 -0600

    Fix key error + lint issues
    There are still lots of line length issues here. The key error
    occurred when a Blazar node did not have a "uid" set.

--- hammers/scripts/clean_old_aggregates.py
+++ hammers/scripts/clean_old_aggregates.py
@@ -1,18 +1,12 @@
-import datetime
+from datetime import datetime
 import pytz
 import sys
 import os
 import itertools
 import re
-import requests
-import traceback
-import dateutil.parser
-from urllib.error import HTTPError
 from hammers.slack import Slackbot
 from hammers import osapi, osrest
 from hammers.osrest.nova import aggregate_delete, _addremove_host
-from hammers.osrest.ironic import nodes
-from hammers.osrest.blazar import hosts,host_allocations,lease
 from hammers.util import base_parser
 from hammers import MySqlArgs, query
 
@@ -28,7 +22,7 @@ mysqlargs = MySqlArgs({
     'password': '',
     'host': 'localhost',
     'port': '3306',
-    })
+})
 mysqlargs.inject(parser)
 args = parser.parse_args(sys.argv[1:])
 auth = osapi.Auth.from_env_or_args(args=args)
@@ -40,17 +34,26 @@ host_allocs = osrest.blazar.host_allocations(auth)
 leases = osrest.blazar.leases(auth)
 dt_fmt = '%Y-%m-%dT%H:%M:%S.%f'
 
+
 def is_terminated(lease):
-
     # Get leases with past end dates
-    now=datetime.datetime.utcnow().replace(tzinfo=pytz.timezone('UTC'))
+    tzinfo = pytz.timezone('UTC')
+    now = datetime.utcnow().replace(tzinfo=tzinfo)
+    lease_end = (
+        datetime.strptime(lease['end_date'], dt_fmt).replace(tzinfo=tzinfo))
+    return now > lease_end
 
-    return now > datetime.datetime.strptime(lease['end_date'], dt_fmt).replace(tzinfo=pytz.timezone('UTC'))
 
-# Match lease to aggregate
 def aggregates_for_lease(lease):
-    physical_reservation_ids = [r['id'] for r in lease['reservations'] if r['resource_type'] == 'physical:host']
-    return [x for x in aggregates.values() if x['name'] in physical_reservation_ids]
+    physical_reservation_ids = [
+        r['id'] for r in lease['reservations']
+        if r['resource_type'] == 'physical:host'
+    ]
+    return [
+        x for x in aggregates.values()
+        if x['name'] in physical_reservation_ids
+    ]
+
 
 def clear_aggregates(agg_list):
     report = []
@@ -62,21 +65,25 @@ def clear_aggregates(agg_list):
                 try:
                     _addremove_host(auth, 'remove_host', x['id'], host)
                     _addremove_host(auth, 'add_host', 1, host)
-                    report.append("Deleted host {} from aggregate {} and returned to freepool. ".format(host, x['id']))
+                    report.append((
+                        f"Deleted host {host} from aggregate {x['id']} and "
+                        "returned to freepool."))
                 except Exception as exc:
-                    report.append("Unexpected error moving host {} from aggregate {} to freepool. ".format(host, x['id']))
+                    report.append((
+                        f"Unexpected error moving host {host} from aggregate "
+                        f"{x['id']} to freepool."))
                     errors.append(exc)
             try:
                 aggregate_delete(auth, x['id'])
-                report.append("Deleted aggregate {}. ".format(x['id']))
+                report.append(f"Deleted aggregate {x['id']}.")
             except Exception as exc:
-                report.append("Unexpected error deleting aggregate {}. ".format(x['id']))
+                report.append(f"Unexpected error deleting aggregate {x['id']}.")
                 errors.append(exc)
 
     return errors, report
 
-def orphan_find(allaggs):
 
+def orphan_find(allaggs):
     # Find all hosts currently in aggregates
     hosts_from_aggs = []
     for agg in allaggs.values():
@@ -84,34 +91,42 @@ def orphan_find(allaggs):
             hosts_from_aggs.append(host)
 
     # Make list of ironic hosts not in any aggregate
-    ironic_hosts = osrest.ironic.nodes(auth, details=False).keys()
+    ironic_nodes = osrest.ironic.nodes(auth, details=False).keys()
+    blazar_hosts = osrest.blazar.hosts(auth).values()
     orphans = []
-    for host in ironic_hosts:
-        if host not in hosts_from_aggs:
-            host_id = [h['id'] for h in osrest.blazar.hosts(auth).values() if h['uid'] == host][0]
+    for node_uuid in ironic_nodes:
+        if node_uuid not in hosts_from_aggs:
+            host_id = next(iter(
+                [h['id'] for h in blazar_hosts if h.get('uid') == node_uuid]
+            ), None)
+            if not host_id:
+                raise ValueError(
+                    f"Node {node_uuid} not associated to any Blazar host!")
             orphans.append(host_id)
     return orphans
 
-def has_active_allocation(orph):
 
-    matching_allocs = [alloc for alloc in host_allocs if alloc['resource_id'] == orph]
+def has_active_allocation(orph):
+    matching_allocs = [
+        alloc for alloc in host_allocs
+        if alloc['resource_id'] == orph
+    ]
     if not matching_allocs:
         return False
     res = matching_allocs[0]['reservations'][0]['id']
     return res
 
-def del_expired_alloc(db, old_alloc):
 
+def del_expired_alloc(db, old_alloc):
     ha_id = old_alloc['id']
     l_id = old_alloc['lid']
-    hh = old_alloc['hypervisor_hostname']
-
-    count = query.blazar_old_host_alloc_delete(db, ha_id)
+    query.blazar_old_host_alloc_delete(db, ha_id)
     return ha_id, l_id
 
+
 def main(argv=None):
-    
-    slack = Slackbot(args.slack, script_name='clean-old-aggregates') if args.slack else None
+    script = 'clean-old-aggregates'
+    slack = Slackbot(args.slack, script_name=script) if args.slack else None
 
     try:
         term_leases = [lease for lease in leases.values() if is_terminated(lease)]
@@ -119,7 +134,7 @@ def main(argv=None):
         aggregate_list = list(itertools.chain(*old_aggregates))
         errors, reports = clear_aggregates(aggregate_list)
         orphan_list = orphan_find(aggregates)
-        
+
         for orphan in orphan_list:
             destiny = has_active_allocation(orphan)
             host = osrest.blazar.host(auth, orphan)
@@ -132,7 +147,6 @@ def main(argv=None):
                 destination_agg = [aggr['id'] for aggr in aggregates.values() if aggr['name'] == destiny][0]
                 reports.append("Moving orphan host {} to destined aggregate {}.".format(orphan, destination_agg))
                 osrest.nova.aggregate_add_host(auth, destination_agg, host['hypervisor_hostname'])
-        
 
         old_allocations = query.blazar_find_old_host_alloc(conn)
         for alloc in old_allocations:
